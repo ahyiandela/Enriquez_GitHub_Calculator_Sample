c898f84143babe9ba66432255fa482ff3e65868b
ahyiandela <quijano.415702@balagtas.sti.edu.ph>
2026-02-11 20:28:31 +0800

    FEAT : add addition

---
+++ Enriquez_GitHub_Calculator_Sample/Addition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enriquez_GitHub_Calculator_Sample
+{
+    public class Addition
+
+    {
+        public static int Addnumber(int num1, int num2)
+        {
+            return num1 + num2;
+
+            
+        }
+    }
+}
